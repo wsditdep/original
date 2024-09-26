74e99836d7aca620a59c1ce258940d24564236b7
Akash-star-design <sakash02700@gmail.com>
2024-09-27 01:27:14 +0700

    Bulid Done

--- app/signin/page.js
+++ app/signin/page.js
@@ -1,38 +1,18 @@
-// import Image from "next/image";
-// import logo from "@/public/auth/logo.png";
-// import Signin from "@/components/auth/Signin";
-// import Link from "next/link";
 
-// export const dynamic = "force-dynamic";
+import Signin from "@/components/auth/Signin";
+import Link from "next/link";
 
-// const page = () => {
-//     return (
-//         <section className="auth-section" style={{
-//             width: '100%',
-//             height: '100%',
-//             // background: `linear-gradient(to bottom, rgb(36 58 84),rgb(107 160 180))`
-//         }}>
+export const dynamic = "force-dynamic";
 
-//             <div className="auth-wrapper">
-//                 <div className="welcome-icon">
-//                     <Link href={"/"}><i className="fa-solid fa-angle-left"></i></Link>
-//                 </div>
-//                 <div className="auth-login-logo">
-//                     <Image
-//                         src={logo}
-//                         height={100}
-//                         width={100}
-//                         alt="logo"
-//                         unoptimized
-//                     />
-//                 </div>
-//                 <Signin />
-//                 <div className="auth-copyright">
-//                     <p><i className="fa fa-shield"></i>We'll never post anything without your permission.</p>
-//                 </div>
-//             </div>
-//         </section>
-//     )
-// }
+const page = () => {
+    return (
+        <section className="auth-section">
 
-// export default page
+
+                {/* <Signin /> */}
+       
+        </section>
+    )
+}
+
+export default page
--- app/signup/page.js
+++ app/signup/page.js
@@ -1,49 +1,21 @@
-// import Link from "next/link";
-// import SignUp from "@/components/auth/SignUp";
-// import Image from "next/image";
-// import logo from "@/public/auth/logo.png";
+import Link from "next/link";
+import SignUp from "@/components/auth/SignUp";
 
 
-// export const dynamic = "force-dynamic"
 
-// const page = () => {
-//     return (
-//         <>
-//             <div className="auth-breadcrumb-logo" >
-//                 <div className="auth-signup-wrapper">
-//                     <div className="auth-signup-logo">
-//                         <Image
-//                             src={logo}
-//                             height={100}
-//                             width={100}
-//                             alt="logo"
-//                             unoptimized
+export const dynamic = "force-dynamic"
 
-//                         />
-//                     </div>
-//                     <div className="auth-signup-cls-btn">
-//                         <Link href="/signin"><i className="fa-solid fa-xmark"></i></Link>
-//                     </div>
-//                 </div>
-//             </div>
-//             <div className="auth-breadcrumb-parent">
-//                 <div className="auth-breadcrumb-childs">
-//                     <h3>Register</h3>
-//                 </div>
-//             </div>
-//             <section className="auth-section">
-//                 <div className="auth-wrapper">
-//                     <div className="register-txt-info">
-//                         <p>Please fill in your details for create an account</p>
-//                     </div>
-//                     <SignUp />
-//                     <div className="auth-copyright mt1">
-//                         <p><i className="fa fa-shield"></i>We'll never post anything without your permission.</p>
-//                     </div>
-//                 </div>
-//             </section>
-//         </>
-//     )
-// }
+const page = () => {
+    return (
+        <>
+          
+            <section className="auth-section">
+           
+                    {/* <SignUp /> */}
+          
+            </section>
+        </>
+    )
+}
 
-// export default page
+export default page
